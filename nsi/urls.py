657506fb696e845d3b2fc7d85ed721fb555be5b4
Derrick Jackson <derrick.jackson73@gmail.com>
2016-01-31 16:47:23 -0500

    second option

--- nsi/urls.py
+++ nsi/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html',)),
+    url(r'^option1/$', TemplateView.as_view(template_name='base.html',), name='option1'),
+    url(r'^option2/$', TemplateView.as_view(template_name='base2.html',), name='option2'),
 
     url(r'^admin/', admin.site.urls),
+
     # Shared Application
     url(r'^shared/', include('shared.urls'), name='shared'),
     url(r'^impersonate/', include('impersonate.urls')),
