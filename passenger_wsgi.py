6032567f8b7b118bd9cd9d3e625d015a06001399
Derrick Jackson <derrick.jackson73@gmail.com>
2016-02-09 17:40:54 -0500

    updated passenger file

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,22 @@
-import sys, os
+import sys
+import os
 
-INTERP = os.path.join(os.environ['HOME'], 'navalsystemsinc', 'bin', 'python')
+INTERP = "/home/nsishell/opt/python3.5.1/bin/python3"
+# INTERP is present twice so that the new python interpreter knows the actual executable path
 
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.append(os.getcwd())
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/nsi')
 
-from app import app as application
+sys.path.insert(0, cwd + '/env/bin')
+sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages/django')
+sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "nsi.settings"
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
