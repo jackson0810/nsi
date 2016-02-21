851e74ccfdde214609bd63f0895ac6af0f8735df
Derrick Jackson <derrick.jackson73@gmail.com>
2016-02-21 15:58:57 -0500

    working on passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,12 +8,9 @@ if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
 cwd = os.getcwd()
-sys.path.append(cwd)
-sys.path.append(cwd + '/nsi')
 
+sys.path.insert(0, cwd + '/nsi')
 sys.path.insert(0, cwd + '/env/bin')
-# sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages/django')
-# sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "nsi.settings.production"
 
@@ -22,17 +19,5 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
-def testapplication(environ, start_response):
-   status = '200 OK'
-   output = 'Hello World! Running Python version ' + sys.version + '\n\n'
-   response_headers = [('Content-type', 'text/plain'),
-                       ('Content-Length', str(len(output)))]
-   # to test paste's error catching prowess, uncomment the following line
-   # while this function is the "application"
-   #raise("error")
-   start_response(status, response_headers)
-   return [output]
-
 application = ErrorMiddleware(application, debug=True)
+application = DjangoWhiteNoise(application)
