33458c33e397a5f55c3aa53a0bb0a878b95bfb81
Derrick Jackson <derrick.jackson73@gmail.com>
2016-02-21 16:05:49 -0500

    working on passenger

--- employees_passenger_wsgi.py
+++ employees_passenger_wsgi.py
@@ -12,27 +12,13 @@ sys.path.append(cwd)
 sys.path.append(cwd + '/nsi')
 
 sys.path.insert(0, cwd + '/env/bin')
-# sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages/django')
-# sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages')
+sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages/django')
+sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "nsi.settings.production_employees"
 
-from paste.exceptions.errormiddleware import ErrorMiddleware
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-def testapplication(environ, start_response):
-   status = '200 OK'
-   output = 'Hello World! Running Python version ' + sys.version + '\n\n'
-   response_headers = [('Content-type', 'text/plain'),
-                       ('Content-Length', str(len(output)))]
-   # to test paste's error catching prowess, uncomment the following line
-   # while this function is the "application"
-   raise("error")
-   start_response(status, response_headers)
-   return [output]
-
-application = ErrorMiddleware(application, debug=True)
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,16 +8,17 @@ if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
 cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/nsi')
 
-sys.path.insert(0, cwd + '/nsi')
 sys.path.insert(0, cwd + '/env/bin')
+sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages/django')
+sys.path.insert(0, cwd + '/env/lib/python3.5/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "nsi.settings.production"
 
-from paste.exceptions.errormiddleware import ErrorMiddleware
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = ErrorMiddleware(application, debug=True)
 application = DjangoWhiteNoise(application)
