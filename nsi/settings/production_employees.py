5e001b4ac4269d8775ea2ee5e50e66f953cdf0ae
Derrick Jackson <derrick.jackson73@gmail.com>
2016-02-21 17:29:32 -0500

    debug was not set to true for the internal site.

--- nsi/settings/production_employees.py
+++ nsi/settings/production_employees.py
@@ -2,8 +2,8 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['employees.navalsystemsinc.com']
-URL_HOME = 'internal'
-URL_HOME_NAME = 'internal'
+URL_HOME = 'external'
+URL_HOME_NAME = 'external'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY_EMPLOYEES']
