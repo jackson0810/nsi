37dcc2966e07966d670666c1d73965166546d3b5
Derrick Jackson <derrick.jackson73@gmail.com>
2016-02-21 15:54:15 -0500

    removed paste from the requirements file

--- nsi/urls.py
+++ nsi/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import url, include, handler404, handler500
 from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
+    url('handler404', 'django.views.defaults.page_not_found'),
+    url('handler500', 'django.views.defaults.server_error'),
     url(r'^', include('{}.urls'.format(settings.URL_HOME), namespace=settings.URL_HOME_NAME)),
 
     url(r'^admin/', admin.site.urls),
