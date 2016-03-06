6d08b91b171c0dc64b52e5eaad9ba77d7db536ab
Derrick Jackson <derrick.jackson73@gmail.com>
2016-03-06 08:21:48 -0500

    adding a logger

--- nsi/settings/base.py
+++ nsi/settings/base.py
@@ -128,3 +128,56 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+
+def ignore_404_errors(record):
+    if record.getMessage():
+        message = str(record.getMessage())
+
+        if 'Not Found:' in message:
+            return False
+
+    return True
+
+LOG_PATH = '/home/nsishell/logs/navalsystemsinc.com/'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] - %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'ignore_404_errors': {
+            '()': 'django.utils.log.CallbackFilter',
+            'callback': ignore_404_errors,
+        }
+    },
+    'handlers': {
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': LOG_PATH + "nsi-error.log",
+            'when': 'midnight',
+            'backupCount': 7,
+            'formatter': 'standard',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false', 'ignore_404_errors'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
