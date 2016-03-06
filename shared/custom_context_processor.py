9ad8af6642cba1113b8d102ee80ac1fbb0709d91
Derrick Jackson <derrick.jackson73@gmail.com>
2016-03-06 08:00:57 -0500

    fixed the home page carousel issue.

---
+++ shared/custom_context_processor.py
@@ -0,0 +1,22 @@
+from django.core.urlresolvers import resolve, Resolver404
+
+
+def custom_proc(request):
+    current_url_name = None
+    current_url_namespace = None
+
+    if request.path_info:
+        try:
+            resolve_result = resolve(request.path)
+
+            if resolve_result:
+                current_url_name = resolve_result.url_name
+                current_url_namespace = resolve_result.namespace
+        except Resolver404:
+            pass
+
+    return {
+        # Makes the URL name available so it can be used to determine which navigation item should be active.
+        'current_url_name': current_url_name,
+        'current_url_namespace': current_url_namespace,
+    }
