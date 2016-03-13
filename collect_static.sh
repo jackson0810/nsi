9082aa9c0f995e4b38191f1f8a6a2c34908d1703
Derrick Jackson <derrick.jackson73@gmail.com>
2016-03-13 11:21:04 -0400

    fixing collect static.

--- collect_static.sh
+++ collect_static.sh
@@ -4,7 +4,7 @@ WORKDIR="/home/nsishell/"
 HOMEDIR="navalsystemsinc/"
 PROJECT="nsi"
 
-cd ${WORKDIR}${HOMEDIR}
+cd ${WORKDIR}${HOMEDIR}${PROJECT}
 
 echo "Collect static ..."
 ${WORKDIR}${HOMEDIR}env/bin/python3 manage.py collectstatic --noinput --settings=nsi.settings.production
