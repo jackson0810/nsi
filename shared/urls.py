ce4b952dfb163dac656265a86460629594d643b1
Derrick Jackson <derrick.jackson73@gmail.com>
2016-03-13 09:28:25 -0400

    created a collect static method URL to handle copying Task Orders that are uploaded.

--- shared/urls.py
+++ shared/urls.py
@@ -1,5 +1,5 @@
-from django.conf import settings
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('shared.views',
+    url(r'^collect/static/$', 'update_static_resources', name='update_static_resources'),
 )
--- shared/views.py
+++ shared/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from shared.utilities import collect_static
+
+
+def update_static_resources(request):
+    collect_static()
+
+    return HttpResponse(status=200)
