26a81dc623ecd731f924caac65768e7c5810277d
Derrick Jackson <derrick.jackson73@gmail.com>
2016-03-13 11:25:19 -0400

    another update to collect_static.

--- collect_static.sh
+++ collect_static.sh
@@ -9,4 +9,7 @@ cd ${WORKDIR}${HOMEDIR}${PROJECT}
 echo "Collect static ..."
 ${WORKDIR}${HOMEDIR}env/bin/python3 manage.py collectstatic --noinput --settings=nsi.settings.production
 
+echo "Restarting external site..."
+touch ${WORKDIR}${HOMEDIR}tmp/restart.txt
+
 echo "Done"
